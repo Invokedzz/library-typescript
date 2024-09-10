b5c7d74aa73c322201e27edbe21d404b08b8e10b
Samuel Nóbrega <162361529+Invokedzz@users.noreply.github.com>
2024-09-10 11:55:26 -0300

    update

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,27 @@
 import { Request, Response } from "express";
 
+import dotenv from "dotenv";
+
 import mysql from "mysql2/promise";
+
+dotenv.config({
+    path: __dirname + 'file.env' });
+
+const userKey = process.env.SQL_USER;
+
+const passKey = process.env.SQL_PASS;
+
+const createPool = mysql.createPool({
+
+    host: "localhost",
+    user: userKey,
+    password: passKey,
+    database: "official_bookstore",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+
+});
 
 export const homepage = (req: Request, res: Response): void => {
     res.render('home');
